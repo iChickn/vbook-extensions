bb83a108f11a1c42b6c180fec294cb021aef3257
LongPT <longpt@psnet.asia>
2021-01-21 16:51:56 +0700

    Fix

--- truyendocnhieu/src/gen.js
+++ truyendocnhieu/src/gen.js
@@ -1,11 +1,12 @@
 function execute(url, page) {
-    if (!page) page = 1;
+    if (!page) page = '1';
     
     const doc = Http.get(url).params({"page": page}).html();
     const booksList = doc.select("#category-books-container > div > ul > li");
     const data = [];
 
-    bookList.forEach(book => {
+    for (var i = 0; i < booksList.size(); i++) {
+        var book = booksList.get(i);
         data.push({
             name: book.select(".box-book-info .name-book").text,
             link: book.select(".box-book-info .name-book").href,
@@ -13,7 +14,6 @@ function execute(url, page) {
             description: book.select(".box-book-info .info-book-des").outerText,
             host: "https://truyendocnhieu.com/"
         });
-    });
-
+    }
     return Response.success(data, page++)
 }
