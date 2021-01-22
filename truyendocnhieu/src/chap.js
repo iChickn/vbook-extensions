f1019be4cbb020cb52861cc8decc3d8d7ddc2b27
LongPT <longpt@psnet.asia>
2021-01-22 11:26:56 +0700

    v20

--- truyendocnhieu/src/chap.js
+++ truyendocnhieu/src/chap.js
@@ -1,8 +1,7 @@
 function execute(url) {
-    var data = [];
     var doc = Http.get(url).string();
     doc = JSON.parse(doc);
-    data.push(doc.chapter.content.replaceAll('</p>', ''));
+    doc = doc.chapter.content.toString();
     
-    return Response.success(doc.chapter.content.replaceAll('</p>', ''));
+    return Response.success(doc.split("</p>").join(""));
 }
