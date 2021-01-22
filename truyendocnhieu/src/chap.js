e73fec53a5824396ca39bb202f3e7b9d6d71424b
LongPT <longpt@psnet.asia>
2021-01-22 10:49:08 +0700

    Fix nội dung chương chứa </p>

--- truyendocnhieu/src/chap.js
+++ truyendocnhieu/src/chap.js
@@ -2,7 +2,7 @@ function execute(url) {
     var data = [];
     var doc = Http.get(url).string();
     doc = JSON.parse(doc);
-    data.push(doc.chapter.content.replaceAll('<p></p>', '\n'));
+    data.push(doc.chapter.content.replaceAll('</p>', ''));
     
     return Response.success(data);
 }
