a18c6f59fae8bb7b15233607711e6117e7605ae3
.Chick <11717659+iChickn@users.noreply.github.com.>
2024-12-18 23:59:03 +0700

    toc.js: Remove decryption

--- sangtacviet/src/toc.js
+++ sangtacviet/src/toc.js
@@ -1,94 +1,65 @@
-function execute(url) {
-  if (url.slice(-1) !== "/") url = url + "/";
-  let host = url.split('/truyen/')[0];
-  const source = url.split('/')[4];
-  const bookId = url.split('/')[6];
+function execute (url) {
+  if (url.slice(-1) !== '/') url = url + '/'
+  const regex = /https:\/\/sangtacviet\.app\/([^\/]+)\/([^\/]+)\/([^\/]+)\/([^\/]+)\//
+  let host = url.split('/truyen/')[0]
+  const source = url.split('/')[4]
+  const bookId = url.split('/')[6]
 
-  let chapListUrl = host + '/index.php?ngmar=chapterlist&h=' + source + '&bookid=' + bookId + '&sajax=getchapterlist&force=true';
-  let list = [];
+  let chapListUrl = host + '/index.php?ngmar=chapterlist&h=' + source +
+    '&bookid=' + bookId + '&sajax=getchapterlist&force=true'
+  let list = []
 
-  var browser = Engine.newBrowser();
-  browser.setUserAgent(UserAgent.android());
-  browser.launch(url, 1000);
-  browser.callJs(`
-document.createElement = function(create) {
-    return function() {
-        var ret = create.apply(this, arguments);
-        if (ret.tagName.toLowerCase() === "a" && arguments.callee.caller.toString().startsWith("function(e,a,b)")) { //
-            arguments.callee.caller.arguments[0].setAttribute("chap-url", "/truyen/${source}/" + arguments.callee.caller.arguments[1] + "/${bookId}/" + arguments.callee.caller.arguments[2] + "/");
-        }
-        return ret;
-    };
-}(document.createElement)
-`, 0);
-
-  browser.callJs("renewchapter(true);", 1000);
-  
-  do {
-    var listchapitems = browser.html().select(".listchapitem");
-  } while (listchapitems.length == 0);
-
-  listchapitems.forEach(chapItem => {
-    let title = chapItem.text();
-    let chapUrl = chapItem.attr("chap-url");
-    list.push({
-        name: title,
-        url: chapUrl,
-        host: host,
-    });
-  });
-
-  return Response.success(list);
-  let response = fetch(chapListUrl, {
+  let resp = fetch(chapListUrl, {
     method: 'GET',
-    headers: {
+    'headers': {
+      'accept': '*/*',
+      'accept-language': 'en-US,en;q=0.9',
+      'cache-control': 'no-cache',
+      'pragma': 'no-cache',
+      'priority': 'u=1, i',
+      'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Windows"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-origin',
+      'cookie': 'hideavatar=false; lang=vi; _acx=UemVEoUiPTTncsFaskENqQ==; _gid=GA1.2.153508239.1734536118; arouting=e; PHPSESSID=3rrm54q8bmr0l21g42mr0rof6h; prefetchAd_3763521=true; _gat_gtag_UA_145395004_1=1; _ga_MNX3PR1HR4=GS1.1.1734536118.2.1.1734537719.0.0.0; _ga=GA1.1.2016460250.1734274737',
       'Referer': url,
+      'Referrer-Policy': 'strict-origin-when-cross-origin',
     },
-    redirect: 'follow',
-  });
+    'body': null,
+  })
 
-  if (response.ok) {
-    let x = response.json();
+  if (resp.ok) {
+    let x = resp.json()
+    try {
+      if (x.code === 1) {
+        if (x.enckey) {
+          eval(atob(x.enckey))
+        }
+        if (!x.data) {
+          return
+        }
+        const cachechapter = x.data.split('-//-')
+        let i = (source === 'uukanshu') ? cachechapter.length - 1 : 0
+        const iend = (i > 0) ? -1 : cachechapter.length
+        for (; i !== iend;) {
+          const da = cachechapter[i].split('-/-')
 
-    load("base64.js");
-    load("stv_deobfuscator.js");
-    load("crypto-js.js");
-    if (x.code == 1) {
-      if (x.enckey) {
-        let decodedEncKey = Base64.decode(x.enckey);
-        let encryptedAes = STV.getEncryptedAes(decodedEncKey, x.data);
-        encryptedAes.encrypted = CryptoJS.enc.Base64.parse(encryptedAes.encrypted);
-        encryptedAes.key = CryptoJS.enc.Utf8.parse(encryptedAes.key);
-        encryptedAes.iv = CryptoJS.enc.Utf8.parse(encryptedAes.iv);
-        let decrypted = CryptoJS.AES.decrypt({ciphertext: encryptedAes.encrypted}, encryptedAes.key, {iv: encryptedAes.iv});
-        x.data = decrypted.toString(CryptoJS.enc.Utf8);
-      }
-      let chapList = x.data.split("-//-");
-      let i = (source === "uukanshu") ? chapList.length - 1 : 0;
-      let iend = (i > 0) ? -1 : chapList.length;
-      for (; i !== iend;) {
-        let chap = chapList[i].split("-/-");
-        let title = "";
-        if (i > 2900) {
-          title = chap[2].replace(/([\t\n]+|<br>|&nbsp;)/g, "").replace(/Thứ ([\d\,]+) chương/i, "Chương $1:").replace(/^(.+)Chương/i, "Chương $1").trim();
-        } else {
-          title = chap[2].replace(/([\t\n]+|<br>|&nbsp;)/g, "").replace(/Thứ ([\d\,]+) chương/, "Chương $1:").trim();
-        }
-        if (source === "uukanshu") {
-          i--;
-        } else {
-          i++;
-        }
-        if (title) {
           list.push({
-            name: title,
-            url: url + chap[1],
+            name: da[2],
+            url: host + '/truyen/dich/' + da[0] + '/28319/' + da[1] + '/',
             host: host,
-          });
+          })
+
+          if (source === 'uukanshu') {
+            i--
+          } else i++
         }
-      }
-    }
+      } else { }
+    } catch (exc) { }
   }
+
   return Response.success(list);
 }
 
